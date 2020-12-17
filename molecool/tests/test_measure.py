c27ba26e302131c8742e1c2ac640980930894078
Jessica Nash <janash@vt.edu>
2020-12-17 13:38:31 -0500

    add unit testing

--- molecool/tests/test_measure.py
+++ molecool/tests/test_measure.py
@@ -3,6 +3,8 @@ Tests for the measure module.
 """
 
 # imports
+import pytest
+
 import molecool
 import numpy as np
 
@@ -11,8 +13,35 @@ def test_calculate_distance():
     r1 = np.array([0, 0, 0])
     r2 = np.array([0, 1, 0])
 
-    expected_distance = 2
+    expected_distance = 1
 
     calculated_distance = molecool.calculate_distance(r1, r2)
 
     assert expected_distance == calculated_distance
+
+# Write a test for the calculate_angle function
+# use points (0, 0, -1) (0, 0, 0) (1, 0, 0)
+# expected angle is 90 degrees
+
+def test_calculate_angle():
+
+    r1 = np.array([1, 0, 0])
+    r2 = np.array([0, 0, 0])
+    r3 = np.array([0, 1, 0])
+
+    expected_value = 90
+
+    calculated_value = molecool.calculate_angle(r1, r2, r3, degrees=True)
+
+    assert pytest.approx(expected_value) == calculated_value
+
+@pytest.mark.parametrize("p1, p2, p3, expected_angle", [
+    (np.array([np.sqrt(2)/2, np.sqrt(2)/2, 0]), np.array([0, 0, 0]), np.array([1, 0, 0]), 45),
+    (np.array([0, 0, -1]), np.array([0, 1, 0]), np.array([1, 0, 0]), 60  ),
+    (np.array([np.sqrt(3)/2, (1/2), 0]), np.array([0, 0, 0]), np.array([1, 0, 0]), 30),
+])
+def test_calculate_angle_many(p1, p2, p3, expected_angle):
+
+    calculated_angle = molecool.calculate_angle(p1, p2, p3, degrees=True)
+
+    assert expected_angle == pytest.approx(calculated_angle), F'{calculated_angle} {expected_angle}'
